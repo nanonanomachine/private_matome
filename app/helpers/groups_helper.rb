6c187a0eb3f4f87bc74a0842aa8c7d6335bb8925
Yohei Koyama <koyama.gakugai@gmail.com>
2014-08-05 16:55:23 +0900

    Add group privacy icon to group name refs #18

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,17 @@
 module GroupsHelper
+	def group_privacy_icon(privacy)
+		content_tag(:i, "", class: privacy_icon_class(privacy))
+	end
+
+	private
+	def privacy_icon_class(privacy)
+		case privacy
+		when "open"
+			"fi-web"
+		when "close"
+			"fi-lock"
+		when "secret"
+			"fi-shield"
+		end
+	end
 end
